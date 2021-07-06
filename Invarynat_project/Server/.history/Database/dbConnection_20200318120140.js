68bf5f346477753c708f0d97fe5f5d7d6700a965
khushahal sharma <khushahal@qsstechnosoft.com>
2021-07-06 17:56:09 +0530

    Delete Invarynat_project directory

--- Invarynat_project/Server/.history/Database/dbConnection_20200318120140.js
+++
@@ -1,9 +0,0 @@
-exports.dbConnection = function () {
-    var dbConfig = {
-        user: "sa", // SQL Server Login
-        password: "thecodehubs", // SQL Server Password
-        server: "DESKTOP-78L71550", // SQL Server Server name
-        database: "Student" // SQL Server Database name
-    };
-    return dbConfig;
-};
--- Invarynat_project/Server/.history/Database/dbConnection_20210706113859.js
+++
@@ -1,9 +0,0 @@
-exports.dbConnection = function () {
-    var dbConfig = {
-        user: "sa", // SQL Server Login
-        password: "Pulsar180", // SQL Server Password
-        server: "RITU-PC", // SQL Server Server name
-        database: "test" // SQL Server Database name
-    };
-    return dbConfig;
-};
