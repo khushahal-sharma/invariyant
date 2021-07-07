41df19fae27c26fce27ae99d8cd0258190d31e8c
khushahal <khushahal@qsstechnosoft.com>
2021-07-07 14:37:29 +0530

    Created getPersonData api for table

--- Invarynat_project/Server/routes/index.js
+++ Invarynat_project/Server/routes/index.js
@@ -24,20 +24,132 @@ router.get("/testApi", function (req, res) {
 });
 
 /* Get All Person */
-router.get("/getPersonData", function (req, res) {
+/*router.get("/getPersonData", function (req, res) {
   sql
     .connect(dbConfig.dbConnection())
-    .then(() => {
-      return sql.query("SELECT Top 100 * FROM Person");
+    .then((pool) => {
+      return sql.query(
+          "SELECT Top 100 * FROM Person Where [RACE] like '%African American%'"
+        )
+      
     })
     .then((result) => {
       console.log(" result ", result);
+      //console.log(" result ", result.recordsets);
 
-      res.json({ data: result.recordset });
+      const preparedResult = [];
+
+      result.recordset.forEach((item) => {
+        preparedResult.push({
+          PERSON_ID: item.PERSON_ID,
+          isAfricanAmerican: item.RACE === "African American" ? "Yes" : "No",
+          isAgeMoreThan40: item.CURRENT_AGE >= 40 ? "Yes" : "NO",
+        });
+      });
+
+      res.json({ data: preparedResult });
     })
     .catch((err) => {
       console.log("Error in query ", err);
     });
+});*/
+
+router.get("/getPersonData", async function (req, res) {
+  try {
+    await sql.connect(dbConfig.dbConnection());
+    /* let result = await sql.query(
+      "SELECT Top 100 * FROM Person Where [RACE] like '%African American%'"
+    );*/
+
+    let result = await sql.query("SELECT Top 100 * FROM Person"),
+      uniquePersonIllnes = {},
+      personIdValues = "";
+
+    // find unique persion id.
+    (result.recordset || []).forEach(
+      ({ PERSON_ID, RACE, CURRENT_AGE } = {}) => {
+        uniquePersonIllnes[PERSON_ID] = {
+          RACE,
+          CURRENT_AGE,
+          PERSON_ID,
+          VISITS: {},
+        };
+
+        // Creating person values;
+        personIdValues += personIdValues.length
+          ? `,${PERSON_ID}`
+          : `${PERSON_ID}`;
+      }
+    );
+
+    //console.log(" personIdValues ", personIdValues);
+
+    let EventResult = await sql.query(
+      `SELECT * FROM Events Where PERSON_ID IN (${personIdValues})`
+    );
+
+    //console.log(" EventResult ", EventResult);
+
+    (EventResult.recordset || []).forEach((item) => {
+      const { EVNET_ID, PERSON_ID, VISIT_ID, EVENT_CD, RESULT_VAL } = item;
+      if (uniquePersonIllnes[PERSON_ID]) {
+        // Add Visit_ID wise data to each person.
+        !uniquePersonIllnes[PERSON_ID]["VISITS"][VISIT_ID] &&
+          (uniquePersonIllnes[PERSON_ID]["VISITS"][VISIT_ID] = {});
+
+        //Add Systolic BP details to Person widget.
+        if (EVENT_CD == 102225120) {
+          uniquePersonIllnes[PERSON_ID]["VISITS"][VISIT_ID][
+            "Resting_Systolic_BP"
+          ] = RESULT_VAL >= 140 ? "Yes" : "No";
+        }
+      }
+    });
+
+    /* Final Object
+    {"Person_ID":{
+      Race: "",
+      Current_AGE;
+      VISTES : {
+        "232":{
+          systolic_BP: 'yes',
+          vist_data: ""
+        },
+        "233":{
+        
+        }
+      }
+    }}
+    
+     */
+
+    //console.log(" uniquePersonIllnes ", uniquePersonIllnes);
+
+    //Prepare final result array from uniquePersonIllnes Object.
+    const preparedResult = [];
+    for (let PersonID in uniquePersonIllnes) {
+      const personDetails = uniquePersonIllnes[PersonID];
+
+      for (let visitId in personDetails["VISITS"]) {
+        const visitDetail = personDetails["VISITS"][visitId];
+
+        preparedResult.push({
+          PERSON_ID: personDetails.PERSON_ID,
+          VISIT_ID: visitId,
+          isAfricanAmerican:
+            personDetails.RACE === "African American" ? "Yes" : "No",
+          isAgeMoreThan40: personDetails.CURRENT_AGE >= 40 ? "Yes" : "No",
+          Resting_Systolic_BP: visitDetail.Resting_Systolic_BP || "NA",
+        });
+      }
+    }
+
+    //console.log(" preparedResult ", preparedResult);
+
+    res.json({ data: preparedResult });
+  } catch (err) {
+    console.log("Error in query ", err);
+  }
 });
 
 /* Get All Visit */
