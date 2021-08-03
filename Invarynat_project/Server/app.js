6127ee3aa9ef138c52735a1e2d02f55c682ca914
hemant-qss2021 <hemant.sharma@qsstechnosoft.com>
2021-08-04 00:44:00 +0530

    added 3 more columns to visitwisepersondiseases table

--- Invarynat_project/Server/app.js
+++ Invarynat_project/Server/app.js
@@ -1,20 +1,35 @@
 "use strict";
 const debug = require("debug"),
   express = require("express"),
-  path = require("path"),
-  favicon = require("serve-favicon"),
   logger = require("morgan"),
   cookieParser = require("cookie-parser");
 /*********************************************** */
-//var express = require("express");
-var router = express.Router();
+const app = express();
+
+const routes = require("./routes/index");
+
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/", routes);
 
 var sql = require("mssql");
 var dbConfig = require("./Database/dbConnection");
 
 const { DiagnosesPointers } = require("./Constant/indexConstants");
 
-//router.get("/runPersonApi", async function (req, res)
 const init = async () => {
   try {
     await sql.connect(dbConfig.dbConnection());
@@ -32,7 +47,6 @@ const init = async () => {
         preparedResult = [],
         uniquePersonIllnes = {};
       // find unique persion id.
-      // (recordSlice || []).forEach(({ PERSON_ID, RACE, CURRENT_AGE } = {}) => {
       uniquePersonIllnes[PERSON_ID] = {
         RACE,
         CURRENT_AGE,
@@ -60,7 +74,7 @@ const init = async () => {
       DiagnosesResult = await sql.query(
         `SELECT * FROM Diagnoses Where PERSON_ID IN (${personIdValues})`
       );
-
+      // console.log(EventResult);
       //preapare Visit map with details.
       let visitIDMap = {};
       (visitResult.recordset || []).forEach((item) => {
@@ -70,7 +84,7 @@ const init = async () => {
       });
 
       (DiagnosesResult.recordset || []).forEach((item) => {
-        let { VALUE, PERSON_ID, VISIT_ID, EVENT_DESC } = item;
+        let { VALUE, PERSON_ID, VISIT_ID, EVENT_DESC, DIAG_DATE } = item;
         if (uniquePersonIllnes[PERSON_ID]) {
           !uniquePersonIllnes[PERSON_ID]["VISITS"][VISIT_ID] &&
             (uniquePersonIllnes[PERSON_ID]["VISITS"][VISIT_ID] = {});
@@ -115,6 +129,7 @@ const init = async () => {
           ) {
             visit["Pre_pregnancy_diagnosis_of_hypertension"] = "Yes";
             visit["Risk_Factor"]["RiskFactor"] += 1;
+            visit["D_Date"] = DIAG_DATE;
           }
         }
       });
@@ -127,6 +142,7 @@ const init = async () => {
           EVENT_CD,
           RESULT_VAL,
           EVENT_DESC = "",
+          EVENT_START_DATE,
         } = item;
 
         EVENT_CD = Number(EVENT_CD);
@@ -182,6 +198,7 @@ const init = async () => {
                 }
               }
             }
+            visit["E_Date"] = EVENT_START_DATE;
           } else if (EVENT_DESC.includes("heart rate")) {
             if (!visit["Resting_HR"]) {
               visit["Resting_HR"] = RESULT_VAL;
@@ -264,11 +281,9 @@ const init = async () => {
           const result = {
             PERSON_ID: Number(personDetails.PERSON_ID),
             VISIT_ID: Number(visitId),
-            // VISIT_NUMBER:
-            //   Number((visitIDMap[visitId] || {}).VISIT_NUMBER) || null,
-            // VISIT_TYPE: (visitIDMap[visitId] || {}).VISIT_TYPE || "",
-            // REG_DAYS_FROM_INDEX:
-            //   (visitIDMap[visitId] || {}).REG_DAYS_FROM_INDEX || "",
+            VISIT_NUMBER: (visitIDMap[visitId] || {}).VISIT_NUMBER || "",
+            D_Date: visitDetail.D_Date || "",
+            E_Date: visitDetail.E_Date || "",
             Risk_Cat: visitDetail.Risk_Cat || "--",
             Risk_Factor: "--",
             History_of_cardiovascular_disease:
@@ -285,7 +300,6 @@ const init = async () => {
             Swelling_in_face_or_hands:
               visitDetail.Swelling_in_face_or_hands || "--",
             Dyspnea: visitDetail.Dyspnea || "--",
-            //Mild_orthopnea: visitDetail.Mild_orthopnea || "--", removed till get full
             Tachypnea: visitDetail.Tachypnea || "--",
             New_or_worsening_headache:
               visitDetail.New_or_worsening_headache || "--",
@@ -303,6 +317,7 @@ const init = async () => {
           if (currentAge >= 40) {
             visitDetail.Risk_Factor.RiskFactor += 1;
           }
+
           if (!personDetails.RACE) {
             if (personDetails.RACE == "african american") {
               visitDetail.Risk_Factor.RiskFactor += 1;
@@ -312,14 +327,15 @@ const init = async () => {
               visitDetail.Risk_Factor.RiskFactor += 1;
             }
           }
-          //  calculating risk category and risk factor count
 
+          //  calculating risk category and risk factor count
           if (visitDetail.Risk_Factor) {
             const { Symptoms, Vitals_sign, RiskFactor, Physical_exam } =
               visitDetail.Risk_Factor;
             let totalRisk = Symptoms + RiskFactor + Vitals_sign + Physical_exam;
-            // console.log('Symptoms',Symptoms,"RiskFactor",RiskFactor,"VitalSign", Vitals_sign,"Physical exam",Physical_exam);
+
             result.Risk_Factor = totalRisk || "--";
+
             if (visitDetail.Risk_Cat !== "RED") {
               result.Risk_Cat =
                 (Symptoms >= 1 && Vitals_sign >= 1 && RiskFactor >= 1) ||
@@ -332,6 +348,7 @@ const init = async () => {
             result.Risk_Cat == "RED" ||
             (result.Risk_Factor && result.Risk_Factor != "--")
           ) {
+            console.log(result);
             preparedResult.push(Object.values(result));
           }
         }
@@ -341,146 +358,137 @@ const init = async () => {
     };
     const loopFunction = async () => {
       let lastProcessedPersonID = 0;
-      // lastperson = await sql.query(
-      //   `select max(person_id) person_id from VisitWisePersonDisease`
+      // let lastperson = await sql.query(
+      // `select max(person_id) person_id from VisitWisePersonDisease`
       // );
       // lastProcessedPersonID = lastperson.recordset[0].person_id;
       // console.log("personid", lastProcessedPersonID);
-      for (let j = 0; j < 2; j++) {
+      for (let j = 0; j < 1; j++) {
         // console.log("batches processed-----", j);
         result = await sql.query(
-          `select PERSON_ID,CURRENT_AGE,isNull(RACE,'') from Person 
-          where cast(person_id as bigint) >${lastProcessedPersonID || 0} 
-          order by cast(person_id as bigint) OFFSET ${offset}  ROWS
-        FETCH NEXT ${interval} ROWS ONLY`
+          `select PERSON_ID,CURRENT_AGE,RACE from Person 
+          where  cast(person_id as bigint) >3143`
+          //   order by cast(person_id as bigint) asc OFFSET ${offset}  ROWS
+          // FETCH NEXT ${interval} ROWS ONLY`
         );
         offset += interval;
-        // interval += 5;
-        console.log("result", result);
 
         let record = result.recordset;
         // console.log("record", record);
         for (let i = 0; i < record.length; i++) {
           let recordSlice = record[i],
             values = await createTableFromPersonID(recordSlice);
-          // console.log("slice", recordSlice);
           // console.log("values", values);
 
           const table = new sql.Table("VisitWisePersonDisease");
           table.create = true;
           table.columns.add("PERSON_ID", sql.BigInt, { nullable: false });
           table.columns.add("VISIT_ID", sql.BigInt, { nullable: false });
-          // table.columns.add("VISIT_NUMBER", sql.BigInt, { nullable: false });
-          table.columns.add("Risk_Cat", sql.VarChar(50), { nullable: true });
+          table.columns.add("VISIT_NUMBER", sql.VarChar(sql.MAX), {
+            nullable: true,
+          });
+          table.columns.add("D_Date", sql.BigInt, { nullable: true });
+          table.columns.add("E_Date", sql.BigInt, { nullable: true });
+          table.columns.add("Risk_Cat", sql.VarChar(sql.MAX), {
+            nullable: true,
+          });
           table.columns.add("Risk_Factor", sql.SmallInt, { nullable: true });
           table.columns.add(
             "History_of_cardiovascular_disease",
-            sql.VarChar(50),
+            sql.VarChar(sql.MAX),
             {
               nullable: true,
             }
           );
-          table.columns.add("Shortness_of_breath_at_rest", sql.VarChar(50), {
-            nullable: true,
-          });
-          table.columns.add("Severe_orthopnea", sql.VarChar(50), {
+          table.columns.add(
+            "Shortness_of_breath_at_rest",
+            sql.VarChar(sql.MAX),
+            {
+              nullable: true,
+            }
+          );
+          table.columns.add("Severe_orthopnea", sql.VarChar(sql.MAX), {
             nullable: true,
           });
           table.columns.add("Resting_HR", sql.SmallInt, { nullable: true });
           table.columns.add("Resting_Systolic_BP", sql.SmallInt, {
             nullable: true,
           });
-          table.columns.add("Oxygen_saturation", sql.VarChar(50), {
+          table.columns.add("Oxygen_saturation", sql.VarChar(sql.MAX), {
             nullable: true,
           });
           table.columns.add("Respiratory_rate", sql.SmallInt, {
             nullable: true,
           });
-          table.columns.add("RACE", sql.VarChar(50), { nullable: true });
+          table.columns.add("RACE", sql.VarChar(sql.MAX), { nullable: true });
           table.columns.add("Age", sql.SmallInt, { nullable: true });
-          table.columns.add("Swelling_in_face_or_hands", sql.VarChar(50), {
+          table.columns.add("Swelling_in_face_or_hands", sql.VarChar(sql.MAX), {
             nullable: true,
           });
-          table.columns.add("Dyspnea", sql.VarChar(50), { nullable: true });
-          table.columns.add("Tachypnea", sql.VarChar(50), { nullable: true });
-          table.columns.add("New_or_worsening_headache", sql.VarChar(50), {
+          table.columns.add("Dyspnea", sql.VarChar(sql.MAX), {
             nullable: true,
           });
-          table.columns.add("Asthma_unresponsive", sql.VarChar(50), {
+          table.columns.add("Tachypnea", sql.VarChar(sql.MAX), {
             nullable: true,
           });
-          table.columns.add("Palpitations", sql.VarChar(50), {
+          table.columns.add("New_or_worsening_headache", sql.VarChar(sql.MAX), {
             nullable: true,
           });
-          table.columns.add("Dizziness_or_syncope", sql.VarChar(50), {
+          table.columns.add("Asthma_unresponsive", sql.VarChar(sql.MAX), {
             nullable: true,
           });
-          table.columns.add("Chest_pain", sql.VarChar(50), { nullable: true });
-          table.columns.add("Loud_murmur_heart", sql.VarChar(50), {
+          table.columns.add("Palpitations", sql.VarChar(sql.MAX), {
+            nullable: true,
+          });
+          table.columns.add("Dizziness_or_syncope", sql.VarChar(sql.MAX), {
+            nullable: true,
+          });
+          table.columns.add("Chest_pain", sql.VarChar(sql.MAX), {
+            nullable: true,
+          });
+          table.columns.add("Loud_murmur_heart", sql.VarChar(sql.MAX), {
             nullable: true,
           });
           table.columns.add(
             "Pre_pregnancy_diagnosis_of_diabetes",
-            sql.VarChar(50),
+            sql.VarChar(sql.MAX),
             {
               nullable: true,
             }
           );
           table.columns.add(
             "Pre_pregnancy_diagnosis_of_hypertension",
-            sql.VarChar(50),
+            sql.VarChar(sql.MAX),
             { nullable: true }
           );
 
-          values.forEach((value) => {
-            table.rows.add(...value);
-          });
+          values.length &&
+            values.forEach((value) => {
+              table.rows.add(...value);
+            });
 
           const request = new sql.Request();
-          let newTable = request.bulk(table, (err, result) => {
-            // ... error checks
-            if (err) {
-              console.log("error", err);
-              // res.json({ error: err });
-            } else if (result) {
-              // console.log(result);
-            }
-          });
+
+          values.length &&
+            request.bulk(table, (err, result) => {
+              // ... error checks
+              if (err) {
+                console.log("error in bulk create", err);
+                // res.json({ error: err });
+              } else if (result) {
+                // console.log(result);
+              }
+            });
         }
       }
     };
-    result = await loopFunction();
+    loopFunction();
     //res.json({ data: "Table created in DB" });
   } catch (err) {
     console.log("Error in query ", err);
   }
 };
 init();
-/*********************************************** */ return;
-const routes = require("./routes/index"),
-  users = require("./routes/users"),
-  dumpTable = require("./routes/dumpTable");
-
-// /prepareDataset
-const app = express();
-
-app.use(logger("dev"));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded());
-app.use(cookieParser());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-app.use("/", routes);
-app.use("/users", users);
-app.use("/", dumpTable);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
